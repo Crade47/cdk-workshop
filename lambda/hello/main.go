aa80dff5bfb74ed05bd1d9643bdf6fd0b3097583
Devansh Soni <devanshsoni79@gmail.com>
2024-02-26 11:06:51 +0530

    first commit

---
+++ lambda/hello/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type App struct {
+	id string
+}
+
+func newApp(id string) *App {
+	return &App{id: id}
+}
+
+func (app *App) Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	responseBody := map[string]string{
+		"message": fmt.Sprintf("Hello from %s!", app.id),
+	}
+
+	responseJson, err := json.Marshal(responseBody)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers: map[string]string{
+				"Content-Type":                "application/json",
+				"Access-Control-Allow-Origin": "*",
+			},
+			Body: "Internal Server Error",
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE",
+		},
+		Body: string(responseJson),
+	}, nil
+
+}
+
+func main() {
+	id := "funcOne"
+	app := newApp(id)
+	lambda.Start(app.Handler)
+}
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,44 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, LambdaRestApi, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    //defining labmda function
+    const helloLambda = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      code: lambda.Code.fromAsset('lambda/hello/bin'),
+      handler: 'bootstrap',
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    const pathLambda = new lambda.Function(this, 'PathHandler', {
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      code: lambda.Code.fromAsset('lambda/path/bin'),
+      handler: 'bootstrap',
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    });
+
+
+    //defining api gateway
+    const api = new RestApi(this, "api", {
+      defaultCorsPreflightOptions: {  
+        allowOrigins: ['*'],
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowHeaders: ['*']
+      } 
+    })
+    api.root.addMethod('ANY')
+
+    //defining resources and methods for hello function
+    const hello = api.root.addResource('hello')
+    hello.addMethod('GET', new LambdaIntegration(helloLambda))
+    hello.addMethod('POST', new LambdaIntegration(helloLambda))
+    //defining resources and methods for path function
+    const path = api.root.addResource('path')
+    path.addMethod('GET', new LambdaIntegration(pathLambda))
+
   }
 }
